62567ec9bf35b08879bd246657d389bb4b5dac88
Vinicios Psidonik <vinicios.psidonik@compasso.com.br>
2020-09-08 09:37:56 -0300

    .

--- schedule1.js
+++ schedule1.js
@@ -539,10 +539,9 @@ const onMessage = e => {
 
                         console.log(`M${timeFrame} / ${direction} / ${activesMapString.has(schedulesArray[1]) ? schedulesArray[1] : schedulesArray[2]} / ${amount} / ${currentTimemmssDate}`);
 
-                        let galeTime = [parseInt(moment(moment().format("YYYY-MM-DD ") + hourmm).utcOffset(0).add(timeFrame * 2, 'm').format('X')), gale && gale.includes('2') ? parseInt(moment(moment().format("YYYY-MM-DD ") + hourmm).utcOffset(0).add(timeFrame * 3, 'm').format('X')) : '']
+                        let galeTime = galeTime = [parseInt(moment(moment().format("YYYY-MM-DD ") + hourmm).utcOffset(0).add(3, 'h').add(timeFrame * 2, 'm').format('X')), gale && gale.includes('2') ? parseInt(moment(moment().format("YYYY-MM-DD ") + hourmm).utcOffset(0).add(3, 'h').add(timeFrame * 3, 'm').format('X')) : '']
 
                         if (digitalPayout && turboPayout && digitalPayout > turboPayout) {
-                            galeTime = [parseInt(moment(moment().format("YYYY-MM-DD ") + hourmm).utcOffset(0).add(3, 'h').add(timeFrame * 2, 'm').format('X')), gale && gale.includes('2') ? parseInt(moment(moment().format("YYYY-MM-DD ") + hourmm).utcOffset(0).add(3, 'h').add(timeFrame * 3, 'm').format('X')) : '']
                             buy(amount, active, direction, parseInt(moment5), timeFrame == 1 ? "PT1M" : "PT5M")
                         } else if (digitalPayout && turboPayout && digitalPayout <= turboPayout) {
                             buy(amount, active, direction, parseInt(moment5), 3)
